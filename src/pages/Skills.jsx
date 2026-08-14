38d9075fc995af8859a26e92b764d70de041f2b5
KAYLEE LIU <KAYLEELIU@UCHICAGO.EDU>
2026-08-14 16:47:16 -0700

    light mode assets

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,17 +2,19 @@ import { forwardRef } from 'react'
 
 const Skills = forwardRef(function Skills(_props, ref) {
     return (
-        <section id="skills" data-section="skills" ref={ref} className="skills-container">
-            <div className="window-parent">
-                <div className="top">Skills.exe</div>
-                <div className="top">_ ■ x</div>
-            </div>
-            <div className="window-text">
-                <b>Development:</b> Python, Git/GitHub, Javascript, HTML, CSS,
-                Node.js, React, C++
-                <br />
-                <br />
-                <b>Design:</b> Adobe Photoshop, Illustrator, InDesign
+        <section id="skills" data-section="skills" ref={ref} className="container">
+            <div className="skills-container">
+                <div className="window-parent">
+                    <div className="top">Skills.exe</div>
+                    <div className="top">_ ■ x</div>
+                </div>
+                <div className="window-text">
+                    <b>Development:</b> Python, Git/GitHub, Javascript, HTML, CSS,
+                    Node.js, React, C++
+                    <br />
+                    <br />
+                    <b>Design:</b> Adobe Photoshop, Illustrator, InDesign
+                </div>
             </div>
         </section>
     )
